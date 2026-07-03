70ca277f5c84c813a1ac1fd3be2ecc3715bf380f
Roy <rholzem@gmail.com>
2026-07-03 14:42:56 +0200

    Inline env vars at build time via next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  env: {
+    HARNESS_ARN: process.env.HARNESS_ARN || '',
+    HARNESS_REGION: process.env.HARNESS_REGION || 'eu-north-1',
+    BEDROCK_MODEL_ID: process.env.BEDROCK_MODEL_ID || '',
+  },
 };
 
 module.exports = nextConfig;
